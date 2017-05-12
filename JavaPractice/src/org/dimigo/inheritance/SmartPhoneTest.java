7a93bd24828d0b256cd07db544385777ab6307b2
김민준 <@DESKTOP-DNOEES4>
2017-05-12 15:46:11 +0900

    실습과제 11 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_SmartPhoneTest
+ *
+ * 1. 개요:
+ * 2. 작성일:2017. 5. 12.
+ * </pre>
+ *
+ * @author : 김민준
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone smartPhones[] = {
+				new IPhone("iPhone 7", "애플", 900000),
+				new Galaxy("갤럭시 S8", "삼성", 800000)
+		};
+		
+		for(SmartPhone s : smartPhones){
+			System.out.println(s.toString());
+			s.turnOn();
+			s.pay();
+			s.useSpecialFunction(s);
+			s.turnOff();
+			System.out.println();
+		}
+	}
+
+}
